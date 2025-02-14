263220828f4eaaf37caa812c8977cd6684ed8bb9
gautrot <alexandre.gautrot@supdevinci-edu.fr>
2025-02-14 11:35:16 +0100

    Maj App

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def bmr_page() -> Response | str:
 
 if __name__ == '__main__':
     app.run(
-        host=os.getenv("FLASK_HOST", 'localhost'),
+        host=os.getenv("FLASK_HOST", '0.0.0.0'),
         port=os.getenv("FLASK_PORT", 5000),
         debug=os.getenv("FLASK_DEBUG", False),
     )
